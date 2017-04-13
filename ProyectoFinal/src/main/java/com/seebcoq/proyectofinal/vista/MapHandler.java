66a308fc9be84979cc2a81bf0fdfd7ddd54034ad
DavidFHCh <davfep_96@hotmail.com>
2017-04-13 15:29:40 -0500

    Corrigiendo cosas de los mapas.

--- ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/vista/MapHandler.java
+++ ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/vista/MapHandler.java
@@ -1,27 +1,33 @@
 package com.seebcoq.proyectofinal.vista;
 
-import java.io.Serializable;
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import javax.faces.bean.ViewScoped;
-
 import org.primefaces.event.map.OverlaySelectEvent;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+/**
+ *
+ * @author miguel
+ */
 @ManagedBean
 @ViewScoped
-public class MapHandler implements Serializable {
+public class MapHandler {
 
     private MapModel advancedModel;
 
     private Marker marker;
 
-    private String title;
+    private String nombre;
 
     private double lat;
 
@@ -30,22 +36,33 @@ public class MapHandler implements Serializable {
     @PostConstruct
     public void init() {
         advancedModel = new DefaultMapModel();
+//        Lat:19.324499386445776, Lng:-99.17937085032463
+//        Lat:19.324328359583355, Lng:-99.17934268712997
 
         //Shared coordinates
-        LatLng coord1 = new LatLng(36.879466, 30.667648);
-        LatLng coord2 = new LatLng(36.883707, 30.689216);
-        LatLng coord3 = new LatLng(36.879703, 30.706707);
-        LatLng coord4 = new LatLng(36.885233, 30.702323);
-
+        //con ayuda de una lista puedes sacar los lugares conocidos de la base de datos y
+        //crear marcadores con dicha info, ojo no usar el addMarker() que ese sirve para agregar lugares a la db.
     }
 
-
-    public String getTitle() {
-        return title;
+    public MapModel getAdvancedModel() {
+        return advancedModel;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void onMarkerSelect(OverlaySelectEvent event) {
+        marker = (Marker) event.getOverlay();
+        System.out.println(marker.getTitle());
+    }
+
+    public Marker getMarker() {
+        return marker;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public double getLat() {
@@ -65,28 +82,12 @@ public class MapHandler implements Serializable {
     }
 
     public void addMarker() {
-        Marker marker = new Marker(new LatLng(lat, lng), title,"helooooo this is something");
+        Marker marker = new Marker(new LatLng(lat, lng), nombre);
+        //aqui se agrega a la base de datos
         advancedModel.addOverlay(marker);
-
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Marker Added", "Lat:" + lat + ", Lng:" + lng));
     }
 
-
-
-
-
-    public MapModel getAdvancedModel() {
-        return advancedModel;
-    }
-
-    public void onMarkerSelect(OverlaySelectEvent event) {
-        marker = (Marker) event.getOverlay();
-    }
-
-    public Marker getMarker() {
-        return marker;
-    }
-    
 }
     
 
