80b01a45bf362ab25a28964fe8396f4792e455a9
DavidFHCh <davfep_96@hotmail.com>
2017-04-15 16:56:40 -0500

    Creado Filtro, te deja navegar en la pagina principal, pero para poder ver mas tienes que iniciar Sesion

---
+++ ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/controlador/FiltroSesion.java
@@ -0,0 +1,57 @@
+package com.seebcoq.proyectofinal.controlador;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = { "*.xhtml" })
+public class FiltroSesion implements Filter {
+
+	public FiltroSesion() {
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+
+			HttpServletRequest reqt = (HttpServletRequest) request;
+			HttpServletResponse resp = (HttpServletResponse) response;
+			HttpSession ses = reqt.getSession(false);
+
+			String reqURI = reqt.getRequestURI();
+
+			if (reqURI.indexOf("/iniciarSesion.xhtml") >= 0
+					|| (ses != null && ses.getAttribute("username") != null)
+					|| reqURI.indexOf("/public/") >= 0
+					|| reqURI.contains("javax.faces.resource")
+          || (reqURI.equals("/ProyectoFinal/"))){
+				chain.doFilter(request, response);
+                        }
+                        else{
+                            //chain.doFilter(request, response);
+				resp.sendRedirect(reqt.getContextPath() + "/faces/iniciarSesion.xhtml");
+                        }
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
