29eb17ffbe6c52c16bb8584680c1992503c281b9
DavidFHCh <davfep_96@hotmail.com>
2017-04-15 22:03:04 -0500

    arreglado problema de inicio de sesion

--- ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/controlador/dao/UsuarioDAO.java
+++ ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/controlador/dao/UsuarioDAO.java
@@ -5,11 +5,11 @@ import com.seebcoq.proyectofinal.controlador.ControladorPersona;
 import com.seebcoq.proyectofinal.modelo.Persona;
 
 public class UsuarioDAO {
-     public static int iniciarSesion(String user, String password) {
+     public static Integer iniciarSesion(String user, String password) {
         ControladorPersona cp = new ControladorPersona();
         List<Persona> lp = cp.buscarUsuario(user,password);
         if(lp == null)
-          return 69;
+          return null;
         Persona p = lp.remove(0);
         boolean admin = p.getEsAdministrador();
         if(admin)
