b86131110b6efb67818c2f614ae365adaa33a967
Palemona <amour.pale@gmail.com>
2017-04-17 04:41:32 -0500

    Se corrigen metodos para que PuestoIH funcione
    se juntaron los metodos de santiago con los de valeria

--- ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/controlador/ControladorPuesto.java
+++ ProyectoFinal/src/main/java/com/seebcoq/proyectofinal/controlador/ControladorPuesto.java
@@ -7,10 +7,13 @@ package com.seebcoq.proyectofinal.controlador;
 import com.seebcoq.proyectofinal.modelo.Comentario;
 import com.seebcoq.proyectofinal.modelo.Puesto;
 import com.seebcoq.proyectofinal.modelo.Persona;
-import com.seebcoq.proyectofinal.modelo.jpaControllers.PuestoJpaController;
-import java.util.List;
+import com.seebcoq.proyectofinal.modelo.jpaControllers.*;
 import javax.persistence.Persistence;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+import com.seebcoq.proyectofinal.modelo.*;
+import java.util.List;
+import java.util.ArrayList;
 import com.seebcoq.proyectofinal.modelo.jpaControllers.PuestoJpaController;
 import com.seebcoq.proyectofinal.modelo.jpaControllers.ComentarioJpaController;
 /**
@@ -19,33 +22,33 @@ import com.seebcoq.proyectofinal.modelo.jpaControllers.ComentarioJpaController;
  */
 public class ControladorPuesto {
     private EntityManagerFactory emf;
-    
+
     public ControladorPuesto(){
         emf = Persistence.createEntityManagerFactory("comidaCienciasPersistentUnit");
     }
-    
+
     public Puesto buscarPuesto(Long id){
         PuestoJpaController puestoCtrl = new PuestoJpaController(emf);
         Puesto puesto = puestoCtrl.findPuesto(id);
         return puesto;
     }
-    
+
     public List<Comentario> buscarComentarios(Puesto puesto){
         return puesto.getComentarioList();
     }
-    
+
     public List<Puesto> buscarPuestos (){
         PuestoJpaController pjc = new PuestoJpaController(emf);
         return pjc.findPuestoEntities();
     }
-    
+
     public Comentario guardarComentario(Puesto puesto, Persona persona, String comentario){
         ComentarioJpaController comentarioCtrl = new ComentarioJpaController(emf);
         Comentario nuevo_comentario = new Comentario(puesto, persona, comentario);
         comentarioCtrl.create(nuevo_comentario);
         return nuevo_comentario;
     }
-    
+
     public void eliminarComentario(Comentario com){
         ComentarioJpaController comentarioCtrl = new ComentarioJpaController(emf);
         try{
@@ -54,9 +57,90 @@ public class ControladorPuesto {
             e.printStackTrace();
         }
     }
-    
+
     public void agregaPuesto(Puesto puesto){
         PuestoJpaController pjc = new PuestoJpaController(emf);
         pjc.create(puesto);
     }
+
+    public List<Menu> buscarMenu(Puesto puesto){
+        List<Menu> menus = new ArrayList<Menu>();
+        menus.addAll(puesto.getMenuList());
+        return menus;
+    }
+
+    public List<Platillo> buscarPlatillos(Puesto puesto){
+        List<Platillo> platillos = new ArrayList<Platillo>();
+        platillos.addAll(puesto.getPlatilloList());
+        return platillos;
+    }
+
+
+    public List<Calificacion> buscarCalificaciones(Puesto puesto){
+        List<Calificacion> calificaciones = new ArrayList<Calificacion>();
+        calificaciones.addAll(puesto.getCalificacionList());
+        return calificaciones;
+    }
+
+    public Double buscarCalificacion(Puesto puesto){
+      Double actual = puesto.getCalificacion();
+      int totalCalfs = buscarCalificaciones(puesto).size();
+      List<Calificacion> calfs = buscarCalificaciones(puesto);
+      for(Calificacion c: calfs){
+          actual = (actual + c.getCalificacion());
+          System.out.println(actual);
+      }
+      actual = (actual)/totalCalfs;
+      puesto.setCalificacion(actual);
+      PuestoJpaController ctrl_puesto = new PuestoJpaController(emf);
+      try{
+      ctrl_puesto.edit(puesto);
+      }catch(Exception e){
+          System.out.println("no se pudo actualizar la calificacion");
+      }
+      return actual;
+    }
+
+    public String buscarImagen(Puesto puesto){
+      String imagen = puesto.getImagen();
+      return imagen;
+    }
+
+
+    public void guardarCalificacion(int c, Puesto puesto, Persona persona){
+      CalificacionJpaController controlador = new CalificacionJpaController(emf);
+      PuestoJpaController ctrl_puesto = new PuestoJpaController(emf);
+      Long idPuesto = puesto.getIdPuesto();
+      Long idUsuario = persona.getIdPersona();
+      System.out.println(idPuesto);
+      System.out.println(idUsuario);
+      System.out.println(c);
+      System.out.println("trabajando");
+      CalificacionPK pk = new CalificacionPK(idPuesto, idUsuario);
+      Calificacion nueva = new Calificacion(pk, c);
+
+     /* try{
+      controlador.create(nueva2);
+      System.out.println("trabajando calificacion");
+      }
+      catch(Exception ex){
+        System.out.println("no se pudo crear la calificacion 2");
+      }*/
+       EntityManager em = emf.createEntityManager();
+      em.getTransaction().begin();
+
+      em.persist(nueva);
+      System.out.println("trabajando calificacion");
+      em.getTransaction().commit();
+      em.close();
+      Double actual = puesto.getCalificacion();
+      actual = (actual + c)/2;
+      puesto.setCalificacion(actual);
+      System.out.println("sigo trabajando");
+       try{
+      ctrl_puesto.edit(puesto);
+       } catch(Exception ex){
+             System.out.println("no se pudo actualizar el puesto");
+       }
+       }
 }
